bd42150818be09d895948f1942419c63d7be3ba0
Jasmeet Narang <jasmeetnrng@gmail.com>
2019-01-07 22:40:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class Decrement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { number: this.props.startNumber };
+    }
+
+    decrement=() => {
+        if (this.state.number < 1) {
+            alert("Cannot be less than zero");
+        } else {
+            this.setState({
+                number: this.state.number - 1
+            });
+        }
+    };
+
+    render() {
+            return (
+                <div className="desc">
+                    <h3>{this.state.number}</h3>
+                    <button onClick={this.decrement}>Click To Decrement</button>
+                    
+                </div>
+            );
+        }
 }
 
+class App extends Component {
+    render() {
+        return (
+            
+            <Decrement startNumber = {10}/>
+           
+            );
+    }
+} 
+
+Decrement.propTypes = {
+    startNumber: PropTypes.number.isRequired
+}
 export default App;
